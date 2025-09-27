26267e11c217a83ebd10c0fdc22f7460d39bc8dc
Jeeva Ann Ebey <jeeva.ds24@duk.ac.in>
2025-09-27 10:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="News Sentiment Dashboard", layout="wide")
 st.title("📰 Real-Time News Sentiment Dashboard")
 
 # Load predictions
-df = pd.read_csv(r"C:\Users\JEEVA ANN EBEY\Downloads\predictions.csv")
+df = pd.read_csv("predictions.csv")
 
 # Map predictions to labels
 df["Sentiment"] = df["predicted_label"].map({0.0: "Negative", 1.0: "Positive"})
@@ -23,3 +23,4 @@ st.dataframe(df[["text", "Sentiment"]], use_container_width=True)
 # Optional: Add chart
 st.subheader("Sentiment Distribution")
 st.bar_chart(df["Sentiment"].value_counts())
+
